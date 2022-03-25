d664856d5c55fbf9b24065caafcc31d4d8ed9200
hectorcamp <hectorcamp@gmail.com>
2022-03-24 23:32:16 -0300

    async/await

--- nodeJS_criando_sua_primeira_biblioteca/index.js
+++ nodeJS_criando_sua_primeira_biblioteca/index.js
@@ -5,13 +5,24 @@ function trataErro(erro) {
   throw new Error(chalk.red(erro.code, 'não há arquivo no caminho'));
 }
 
-function pegaArquivo(caminhoDoArquivo) {
+async function pegaArquivo(caminhoDoArquivo){
   const encoding = 'utf-8';
-  fs.promises
-  .readFile(caminhoDoArquivo, encoding)
-  .then((texto) => console.log(texto))
-  .catch((erro) => trataErro(erro))
+  try {
+    const texto = await fs.promises.readFile(caminhoDoArquivo, encoding)
+    console.log(chalk.green(texto))
+  } catch(erro) {
+    trataErro(erro);
+  }
+  
 }
+
+// function pegaArquivo(caminhoDoArquivo) {
+//   const encoding = 'utf-8';
+//   fs.promises
+//   .readFile(caminhoDoArquivo, encoding)
+//   .then((texto) => console.log(texto))
+//   .catch((erro) => trataErro(erro))
+// }
 // function pegaArquivo(caminhoDoArquivo) {
 //   const encoding = 'utf-8'
 //   fs.readFile(caminhoDoArquivo, encoding, (erro, texto) => {
