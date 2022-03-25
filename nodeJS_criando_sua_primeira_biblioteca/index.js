fcaf3e10b49bdbbe79022558f8b743ff4acea0aa
hectorcamp <hectorcamp@gmail.com>
2022-03-24 23:09:05 -0300

    Tratamento de erros

--- nodeJS_criando_sua_primeira_biblioteca/index.js
+++ nodeJS_criando_sua_primeira_biblioteca/index.js
@@ -1,12 +1,18 @@
 const chalk = require('chalk');
 const fs = require	('fs');
 
-function pegaArquivo(caminhoDoArquivo) {
-  const encoding = 'utf-8'
-  fs.readFile(caminhoDoArquivo, encoding, (_, texto) => {
-    console.log(chalk.green(texto));
-  } )
+function trataErro(erro) {
+  throw new Error(chalk.red(erro.code, 'não há arquivo no caminho'));
 }
 
-pegaArquivo('./arquivos/texto1.md')
+function pegaArquivo(caminhoDoArquivo) {
+  const encoding = 'utf-8'
+  fs.readFile(caminhoDoArquivo, encoding, (erro, texto) => {
+    if (erro) {
+      trataErro(erro);
+    }
+    console.log(chalk.green(texto));
+  })
+}
 
+pegaArquivo('./arquivos/texto1.md');
