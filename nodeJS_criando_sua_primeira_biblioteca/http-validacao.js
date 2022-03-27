bc8088a484a6d76548dd2b35098c2b7e362e2794
hectorcamp <hectorcamp@gmail.com>
2022-03-27 02:31:11 -0300

    Montando o Objeto

--- nodeJS_criando_sua_primeira_biblioteca/http-validacao.js
+++ nodeJS_criando_sua_primeira_biblioteca/http-validacao.js
@@ -1,8 +1,19 @@
+const fetch = require('node-fetch');
+
+async function checaStatus(arrayURLs) {
+    // promise async await
+    const arrayStatus = await Promise.all(arrayLinks.map(url => {
+        const res = await fetch(url)
+        return res.status;
+    }))
+    return arrayStatus;
+}
+
 function geraArrayDeURLs(arrayLinks) {
     // loop para cada {chave : valor}
     // objeto -> [valor]
     //Object.values(objeto)
-    return arrayLinks.map(objetoLink => Object.values(objetoLink))
+    return arrayLinks.map(objetoLink => Object.values(objetoLink).join());
 }
 
 function validaURLs(arrayLinks) {
