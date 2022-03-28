e53489d76e0eb463b3f48d3b7af77455802cf578
hectorcamp <hectorcamp@gmail.com>
2022-03-28 00:49:41 -0300

    Adicionando testes

--- nodeJS_criando_sua_primeira_biblioteca/test/index.test.js
+++ nodeJS_criando_sua_primeira_biblioteca/test/index.test.js
@@ -1,5 +1,10 @@
+const { it } = require('eslint/lib/rule-tester/rule-tester');
 const pegaArquivo = require('../index');
 
-test('deve ser uma função', () => {
-    expect(typeof pegaArquivo).toBe('function');
-  });
+describe('pegaArquivo::', () => {
+    it('deve ser uma função', () => {
+        expect(typeof pegaArquivo).toBe('function');
+    })
+    it('deve retornar array de resultados')
+})
+//parei aqui
